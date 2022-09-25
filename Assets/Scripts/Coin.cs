9d5a53f9023a4ec4a9ff73a2a70235604e676c60
Victor (Vi) <vitugazi17092002@gmail.com>
2022-09-25 19:19:46 +0300

    [Fix] PickUp coins

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -9,11 +9,12 @@ public class Coin : MonoBehaviour
 {
     [SerializeField] private float time = 0.5f;
 
-    private bool canPickUp = false;
-
     private void Start()
     {
-        Observable.Timer(System.TimeSpan.FromSeconds(time)).TakeUntilDisable(gameObject).Subscribe(_ => canPickUp = true);
+        Observable.Timer(System.TimeSpan.FromSeconds(time)).TakeUntilDisable(gameObject).Subscribe(_ => 
+        {
+            gameObject.layer = 7;
+        });
     }
 
     public void Collect()
@@ -23,7 +24,7 @@ public class Coin : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.TryGetComponent(out PlayerMovement _) && canPickUp)
+        if (collision.collider.TryGetComponent(out PlayerMovement _))
         {
             Collect();
         }
